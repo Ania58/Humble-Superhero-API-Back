03142fc2d29ae15dd8c3d6f44b1def683ef8a7fe
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-31 00:32:26 +0100

    require and use cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 const superheroes = [];
 
+app.use(cors()); 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
 
